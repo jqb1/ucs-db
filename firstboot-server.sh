90199bcdb9a42fbd0e531d7caf61a559650cf02a
Kamil Janiec <pastime.mail@gmail.com>
2018-05-13 00:15:16 +0200

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- firstboot-server.sh
+++ firstboot-server.sh
@@ -3,37 +3,22 @@
 [[ "${EUID}" -ne "0" ]] \
   && { echo 'Root permisions required.' 1>&2; exit 1; }
 
-yum install wget -y
+rpm -ivh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 
-# Download and add the repository, then update.
-wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum update
-
-rm -f ./mysql-community-release-el7-5.noarch.rpm
-
-# Install MySQL and start the service.
+yum update -y
 yum install mysql-server -y
+
 systemctl start mysqld
 
-# Very secure way of storing a passwords.
-ROOT_PASSWORD=
+mysql -u root < ./sql/clean_mysql_server.sql
+mysql -u root < ./sql/create_ucs_database.sql
+# mysql -u root < ./sql/init_ucs_database.sql
 
-# Create database before password assigned.
-mysql -u root < ./used_cars_store.sql
-
-# Set up password, able remote connection,
-# and remove unnecessary things.
 mysql -u root -e "$(cat << END
-UPDATE mysql.user SET Password=PASSWORD('${ROOT_PASSWORD}') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\\\_%';
-
-CREATE USER 'root'@'%' IDENTIFIED BY '${ROOT_PASSWORD}';
+CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-
-FLUSH PRIVILEGES;
+UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
+FLUSH PRIVILEGES
 END
 )"
 
