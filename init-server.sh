95ecc5dc7093a74592c10b352f22849f8383caa9
Kamil Janiec <pastime.mail@gmail.com>
2018-05-20 00:53:55 +0200

    Derp. Fix missing constant post rename

--- init-server.sh
+++ init-server.sh
@@ -16,6 +16,9 @@ service docker restart
 PERSISTANCE_STORAGE='database'
 INTERNAL_NETWORK='lorehaven'
 
+MYSQL='database'
+MYSQL_ROOT_PASSWORD='bazdan#20'
+
 # These ports does matter.
 MYSQL_OUTER_PORT='3306'
 FLASK_OUTER_PORT='80'
@@ -26,12 +29,23 @@ docker volume create "${PERSISTANCE_STORAGE}"
 docker network create "${INTERNAL_NETWORK}"
 
 docker run \
--e MYSQL_ROOT_PASSWORD='bazdan#20' \
+-e MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}" \
 -v "${PERSISTANCE_STORAGE}":/var/lib/mysql \
 --network="${INTERNAL_NETWORK}" \
+--name="${MYSQL}" \
 -p "${MYSQL_OUTER_PORT}":3306 \
 -d \
 mysql:5.7
+
+docker run \
+-e USER='root' \
+-e PASSWORD="${MYSQL_ROOT_PASSWORD}" \
+-e HOST="${MYSQL}" \
+-v "${PWD}/mysql":/sql \
+--network="${INTERNAL_NETWORK}" \
+-rm \
+-d \
+theredfoxlee/mysql-exec:1.0
 
 docker run \
 -v "${PWD}/flask":/app \
