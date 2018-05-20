ff424949dbe28325773cea3ac992d28a5b662308
Kamil Janiec <pastime.mail@gmail.com>
2018-05-20 02:19:24 +0200

    Added translation.

--- init-server.sh
+++ init-server.sh
@@ -3,55 +3,12 @@
 [[ "${EUID}" -ne "0" ]] \
   && { echo 'Root permisions required.' 1>&2; exit 1; }
 
-# Make sure you've got docker entry in your apt.
-# Install docker engine afterwards.
 apt-get update
-apt-get install -y docker.io
 
-# It's weird, but docker daemon starts up right away,
-# so just in case, re-start it.
+apt-get install -y docker.io
+apt-get install -y docker-compose
+
 service docker restart
 
-# These names does not matter.
-PERSISTANCE_STORAGE='database'
-INTERNAL_NETWORK='lorehaven'
-
-MYSQL='database'
-MYSQL_ROOT_PASSWORD='bazdan#20'
-
-# These ports does matter.
-MYSQL_OUTER_PORT='3306'
-FLASK_OUTER_PORT='80'
-
-# Do the docker magic.
-
-docker volume create "${PERSISTANCE_STORAGE}"
-docker network create "${INTERNAL_NETWORK}"
-
-docker run \
--e MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}" \
--v "${PERSISTANCE_STORAGE}":/var/lib/mysql \
---network="${INTERNAL_NETWORK}" \
---name="${MYSQL}" \
--p "${MYSQL_OUTER_PORT}":3306 \
--d \
-mysql:5.7
-
-sleep 10
-
-docker run \
--e USER='root' \
--e PASSWORD="${MYSQL_ROOT_PASSWORD}" \
--e HOST="${MYSQL}" \
--v "${PWD}/mysql":/sql \
---network="${INTERNAL_NETWORK}" \
--d \
-theredfoxlee/mysql-exec:1.0
-
-docker run \
--v "${PWD}/flask":/app \
---network="${INTERNAL_NETWORK}" \
--p "${FLASK_OUTER_PORT}":80 \
--d \
-theredfoxlee/flask:1.0
+docker-compose up -d
 
