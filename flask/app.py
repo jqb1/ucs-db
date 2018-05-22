1a2aa81551aedcb6434033ce8424e3eedd066754
Kamil Janiec <pastime.mail@gmail.com>
2018-05-22 21:41:07 +0200

    Push poorly written test can down the road another ten years

--- flask/app.py
+++ flask/app.py
@@ -3,11 +3,15 @@ from flask import \
   redirect, \
   url_for, \
   render_template, \
-  request, \
-  flash
+  request
+
 
 import db
-import db_test
+
+
+# Temporary solution. 
+# We'll use flask session later.
+CURRENT_USER = 'Unknown'
 
 app = Flask(__name__)
 db_handle = db.connect(username='root', \
@@ -20,7 +24,7 @@ def home():
 
 
 @app.route("/login")
-def login(state=None):
+def login():
   return render_template('login.html')
 
 
@@ -30,31 +34,36 @@ def validate():
   password = request.form['password']
 
   if db.validate_user(db_handle, username, password):
-    #return render_template('view.html')
-    # return "Hello {}!".format(username)
+    CURRENT_USER = username
     return redirect(url_for('view'))
   else:
     return redirect(url_for('login'))
 
+
 @app.route("/view", methods=['GET', 'POST'])
 def view():
-  cars = db.fetch_cars(db_handle) 
-  print(cars)
+  cars = db.fetch_cars(db_handle)
   return render_template('view.html', \
-    cars=cars)
+    cars=cars, user=CURRENT_USER)
 
-@app.route("/cars", methods=['GET', 'POST'])
-@app.route("/cars/<brand>")
-def cars_list(brand=None):
-  if request.method == 'POST':
-    brand = request.form['brand']
-    return redirect('/cars/{}'.format(brand))
 
-  cars = db_test.get_cars(brand)
-  return render_template('list_cars.html', \
-    cars_data=cars, brand=brand)
+@app.route("/filter", methods=['POST'])
+def filter():
+  # Filter form handler for view.html.
+  # brand = request.form['brand']
+  # ...
+  # create filter variable x for fetch_cars
+  # x = ...
+  # cars = fetch_cars(db, x)
+  # return redirect(url_for(view), cars)
+  return redirect(url_for('view'))
 
 
 if __name__ == '__main__':
-  app.run(host="localhost", port="80", debug=True)
+  app.run(host="0.0.0.0", port="80")
+  # If you want to test it locally, go with
+  # app.run(host="localhost", port="80", debug=True)
+  # and change database host above to server's IP
+  # eg. db.connect(username='root', \
+  # password='bazdan#20', host='182.321.142.999')
 
