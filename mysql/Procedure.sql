a3d50c80b75934d9eff096c55cf0d56f8d8b831c
Jakub Kozioł <jakub.koziol12@gmail.com>
2018-06-06 17:39:46 +0200

    restore

--- mysql/Procedure.sql
+++ mysql/Procedure.sql
@@ -1,11 +1,10 @@
-CREATE PROCEDURE buyCar(IN id_account INT, IN id_car INT)
+CREATE PROCEDURE 'buyCar'(IN id_account INT, IN id_car INT)
 BEGIN
 UPDATE card
-SET balance=(SELECT price FROM car where id=id_car);
+SET expenses=(SELECT price FROM car where id=id_car);
 INSERT INTO transaction(account_id, car_id, execution_date)
 VALUES(id_account, id_car,CURDATE());
 UPDATE car
-SET status='sold_out'
-WHERE id=id_car;
+SET status='sold_out';
 END;
 
